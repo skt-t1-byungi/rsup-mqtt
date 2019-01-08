9759521ec5d4bc8bb19cf9f9b4b439d902c5fe25
skt-t1-byungi <tiniwz@gmail.com>
2019-01-08 13:32:37 +0900

    remove object spread

--- src/pahoConnect.js
+++ src/pahoConnect.js
@@ -1,9 +1,12 @@
-export default function (paho, pahoOpts = {}) {
+import xtend from 'xtend/immutable'
+
+export default function (paho, opts = {}) {
     return new Promise((resolve, reject) => {
-        paho.connect({
-            ...pahoOpts,
-            onSuccess: resolve,
-            onFailure: error => reject(error)
-        })
+        paho.connect(
+            xtend(opts, {
+                onSuccess: resolve,
+                onFailure: error => reject(error)
+            })
+        )
     })
 }
