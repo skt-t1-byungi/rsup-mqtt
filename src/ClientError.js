e23afd1011993f65b06bb55ee98bb59f16940ef0
skt-t1-byungi <tiniwz@gmail.com>
2019-01-17 15:57:18 +0900

    error methods move to props

--- src/ClientError.js
+++ src/ClientError.js
@@ -1,24 +1,8 @@
 export default class ClientError extends Error {
     constructor (code, message) {
         super(message)
-
-        this._code = code
-        this._message = message
-    }
-
-    get code () {
-        return this._code
-    }
-
-    get message () {
-        return this._message
-    }
-
-    is (code) {
-        return this._code === code
-    }
-
-    occurred () {
-        return this._code !== 0
+        this.code = code
+        this.is = code => this.code === code
+        this.occurred = () => this.code === 0
     }
 }
