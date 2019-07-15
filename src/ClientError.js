217fc41a0c1afa5f443bfc70c61a4bcacb489e84
skt-t1-byungi <tiniwz@gmail.com>
2019-07-15 17:59:31 +0900

    fix add error name

--- src/ClientError.js
+++ src/ClientError.js
@@ -1,6 +1,7 @@
 export default class ClientError extends Error {
     constructor (code, message) {
         super(message)
+        this.name = 'ClientError'
         this.code = code
         this.is = code => this.code === code
         this.occurred = () => this.code !== 0
